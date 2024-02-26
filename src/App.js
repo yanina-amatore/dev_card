8830f00d1e022277c434cc30437e21d18fc7500b
Yanina Amatore <yanina.amatore@datacom.co.nz>
2024-02-27 09:58:27 +1300

    Creating dev-card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const skillsList= ['HTML+CSS', 'JavaScript', 'Web Design', 'Git & Github', 'React', 'Svelte' ]
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card' >     
+      <body>
+        <Image img = {'dev_photo.jpg'} alt="dev_photo"/>
+        <DeveloperInfo 
+        name={"Jonas Schmedtmann"} 
+        text= {'Full-stack web dveloper and teacher ay Udemy. When not coding or preparing a course, I lie to play board games, to cook (and eat), or to just enjoy the Portuguese sun at the beach.'}/>      
+        <SkillList skill={skillsList}/>
+      </body>
     </div>
   );
 }
 
-export default App;
+function Image(props){
+  return(
+    <div className='avatar'>
+      <img src={props.img}alt={props.alt} />
+    </div>
+  )
+}
+
+function DeveloperInfo(props){
+  return(
+    <div className='data'>
+      <h1>{props.name}</h1>
+      <p>{props.text}</p>
+    </div>
+  )
+}
+
+function SkillList(props){
+ 
+
+  return(
+    <div className='skill-list'>
+      <h1 className='skill'>{props.skill}</h1>
+    </div>
+  )
+}
