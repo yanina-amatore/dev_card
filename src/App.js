7e6f1f06c63ceeb26be05876168c2e5acb288e89
Yanina Amatore <yanina.amatore@datacom.co.nz>
2024-02-28 10:24:25 +1300

    tidy up CSS

--- src/App.js
+++ src/App.js
@@ -1,56 +1,53 @@
-import './App.css';
+import './App.css'
 
 // const skillsList= ['HTML+CSS', 'JavaScript', 'Web Design', 'Git & Github', 'React', 'Svelte' ]
 
 export default function App() {
   return (
-    <div className='card' >     
-      <body>
-        <Image img = {'dev_photo.jpg'} alt="dev_photo"/>
-        <DeveloperInfo 
-        name={"Jonas Schmedtmann"} 
-        text= {'Full-stack web dveloper and teacher ay Udemy. When not coding or preparing a course, I lie to play board games, to cook (and eat), or to just enjoy the Portuguese sun at the beach.'}/>      
+    <div className="card">
+      <Image img={'dev_photo.jpg'} alt="dev_photo" />
+      <div className="data">
+        <DeveloperInfo
+          name={'Jonas Schmedtmann'}
+          text={
+            'Full-stack web dveloper and teacher ay Udemy. When not coding or preparing a course, I lie to play board games, to cook (and eat), or to just enjoy the Portuguese sun at the beach.'
+          }
+        />
         <SkillList />
-      </body>
-    </div>
-  );
-}
-
-function Image(props){
-  return(
-    <div className='avatar'>
-      <img src={props.img}alt={props.alt} />
+      </div>
     </div>
   )
 }
 
-function DeveloperInfo(props){
-  return(
-    <div className='data'>
+function Image(props) {
+  return <img className="avatar" src={props.img} alt={props.alt} />
+}
+
+function DeveloperInfo(props) {
+  return (
+    <div>
       <h1>{props.name}</h1>
       <p>{props.text}</p>
     </div>
   )
 }
 
-function SkillList(){ 
-  return(
-    <div className='skill-list'>
-      <Skill skill='HTML+CSS' color= 'red'/> 
-      <Skill skill='Web Design' color= 'orange'/>
-      <Skill skill='Git & Github' color= 'yellow'/>
-      <Skill skill='React'color= 'blue'/>
-      <Skill skill='Svelte'color= 'pink'/>
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML+CSS" color="red" />
+      <Skill skill="Web Design" color="orange" />
+      <Skill skill="Git & Github" color="yellow" />
+      <Skill skill="React" color="blue" />
+      <Skill skill="Svelte" color="pink" />
     </div>
   )
 }
 
-function Skill(props){ 
-
-  return(
-    <div className='skill' style={{backgroundColor: props.color}}>
-      <span >{props.skill}</span>
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
     </div>
   )
 }
-
